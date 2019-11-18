e9027f9e02752b6a8a9c3eef2f48a9addc50471f
davidhennig <davidjhennig@gmail.com>
2019-11-18 12:53:05 -0500

    first

---
+++ src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <section className="login-page">
+      <header>
+        <h1>Login Here</h1>
+      </header>
+    </section>
+  );
+}
---
+++ src/Components/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <section className="signup-page">
+      <header>
+        <h1>SIG UP!</h1>
+      </header>
+    </section>
+  );
+}
